b18a6ced8070a8b4d79558b3aefe8cd9e6a84652
Andrew Smith <andrew.c.smith289@gmail.com>
2020-01-20 15:13:55 -0500

    Added splash section with quote

---
+++ js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    const coll = document.querySelector(".collapse");
+    const content = document.querySelector("header nav ul");
+    const mainElement = document.querySelector("main");
+
+    coll.addEventListener("click", function() {
+        this.classList.toggle("active");
+        
+        if (content.style.display === "flex") {
+        content.style.removeProperty("display");
+        mainElement.style.removeProperty("margin-top"); 
+        
+        } else {
+        content.style.display = "flex";
+        mainElement.style.marginTop = "186px";
+        }
+    });
+
+    window.addEventListener("resize", function() {
+        mainElement.style.removeProperty("margin-top"); 
+            
+        const innerWidth = window.innerWidth;
+        if (innerWidth > 375) {
+          content.style.display = "flex";
+        } else {
+          content.style.display = "none";      
+        }
+      });
+});
+
+
+
